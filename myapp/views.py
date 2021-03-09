c33ec18ea495ebd1c5d7d77d64a34b0b109e6f63
CJCU-CC <108b32710@mailst.cjcu.edu.tw>
2021-03-09 11:13:13 +0800

    commit push

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404
 # Create your views here.
+def about(request):
+    return render(request, 'about.html', locals())
