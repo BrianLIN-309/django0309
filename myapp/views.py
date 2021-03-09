ae56cf7cb6da0a3fcc31bc765d06772e05467b3e
CJCU-CC <108b32710@mailst.cjcu.edu.tw>
2021-03-09 11:38:02 +0800

    commit push

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from myapp.models import Product
 # Create your views here.
 def about(request):
-    return render(request, 'about.html', locals())
+    html ='''<!DOCTYPE html>
+        <html><head><title>About Myself</title></head>
+        <body><h2>Min-Huang Ho</h2><hr>
+        <p>hi i am 113245465</p>
+        </body>
+        </html>
+        '''
+def listing(request):
+    products = Product.objects.all()
+    return render(request, 'list.html', locals())
